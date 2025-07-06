3a3e26b938c8993426f0da6479e2911aa9214794
Léo Borges <leo_borgess@live.com>
2025-07-06 13:26:14 -0300

    First commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+    return (
+        <h2>Bem-vindo ao meu bookstore!</h2>
+    )
+}
+
+export default HomePage;
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+export const NotFoundPage = () => {
+    return (
+        <h3>404: Not Found!</h3>
+    );
+};
+
+export default NotFoundPage;
